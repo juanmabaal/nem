316a146e285cff78fb7471fc30f624cf1687b055
juanmabaal <juanma.baal@gmail.com>
2023-06-27 15:09:47 -0500

    Implementación de estado de carga: Loading skeletons, al recargar la página

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,6 +2,9 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoCounter } from '../TodoConter';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos} from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
 import imagen from '../TODO ES POSIBLE.png'
 
@@ -28,9 +31,9 @@ function AppUI({
           />
     
           <TodoList>
-            {loading && <p>Estamos cargando...</p>}
-            {error && <p>Cuidado, tienes un error!!</p>}
-            {(!loading && searchedTodos.length === 0) && <p>¡Crea tu primer TODO!</p>}
+            {loading && <TodosLoading />}
+            {error && <TodosError />}
+            {(!loading && searchedTodos.length === 0) && <EmptyTodos/>}
             {searchedTodos.map(todo => (
               <TodoItem
                 key={todo.text}
